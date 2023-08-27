cf82e4ba2cb5eef992329a4aa12ce1aeb04d8d43
Pedro Henrique <ph6704938@gmail.com>
2023-08-26 23:36:23 -0300

    Testando metodos

--- src/exerciciosDeFixacao/orientacaoAObjeto/aplication/e1.java
+++ src/exerciciosDeFixacao/orientacaoAObjeto/aplication/e1.java
@@ -14,11 +14,14 @@ public class e1 {
 
         Rectangle rectangle = new Rectangle();
 
-        System.out.println("Enter rectangle width and height");
+        System.out.println("Enter rectangle width and height: ");
         rectangle.height = sc.nextDouble();
         rectangle.width = sc.nextDouble();
 
-        System.out.println(rectangle.height + ", " + rectangle.width);
+        System.out.printf("Area = %.2f%n", rectangle.area());
+        System.out.printf("Perimeter = %.2f%n", rectangle.perimeter());
+        System.out.printf("Diagonal = %.2f%n", rectangle.diagonal());
 
+        sc.close();
     }
 }
