8d42c60d08e299a4ca28a83d9df49c0ddca6e33b
Pedro Henrique <ph6704938@gmail.com>
2023-08-27 12:59:30 -0300

    Criando metodos na classe Employee

--- src/exerciciosDeFixacao/orientacaoAObjeto/entities/Employee.java
+++ src/exerciciosDeFixacao/orientacaoAObjeto/entities/Employee.java
@@ -7,4 +7,17 @@ public class Employee {
     public double tax;
 
 
+    public double netSalary(){
+        return this.grossSalary - this.tax;
+    }
+
+    public void increaseSalary(double percentage){
+        this.grossSalary *= percentage / 100;
+    }
+
+    @Override
+    public String toString(){
+        return this.name + ", $ " + String.format("%.2f", netSalary());
+    }
+
 }
