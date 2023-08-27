0086d0bf719ad39dece42fed9ec67331cc20d070
Pedro Henrique <ph6704938@gmail.com>
2023-08-26 22:06:05 -0300

    Testando o objeto produto

--- src/application/Problema2ComOO.java
+++ src/application/Problema2ComOO.java
@@ -1,7 +1,27 @@
 package application;
 
+import entities.Product;
+
+import java.util.Scanner;
+
 public class Problema2ComOO {
 
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        Product product = new Product();
+
+        System.out.println("Enter product data: ");
+
+        product.name = sc.next();
+        product.price = sc.nextDouble();
+        product.quntity = sc.nextInt();
+
+        System.out.printf(product.name + " " + "%.2f" + " " + product.quntity, product.price);
+
+        sc.close();
+    }
 
 
 }
