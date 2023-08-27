16498760f9407732008061995878a4e5d84df449
Pedro Henrique <ph6704938@gmail.com>
2023-08-26 22:30:40 -0300

    Adicionando o Locale default como US

--- src/application/Problema2ComOO.java
+++ src/application/Problema2ComOO.java
@@ -2,12 +2,14 @@ package application;
 
 import entities.Product;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Problema2ComOO {
 
     public static void main(String[] args) {
 
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
         Product product = new Product();
