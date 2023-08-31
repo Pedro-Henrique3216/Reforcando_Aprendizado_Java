0d3efa92fffaeefaa97f6801f77190b10a431d03
Pedro Henrique <ph6704938@gmail.com>
2023-08-31 10:56:21 -0300

    Atualizando classe Membros estaticos para V3__ MembroEstatico com ela usando o calculator com metodos e atributos estaticos exemplo

--- src/exemplosDeAula/MembrosEstaticos.java
+++ src/exemplosDeAula/MembrosEstaticos.java
@@ -12,18 +12,17 @@ public class MembrosEstaticos {
 
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        Calculator calculator = new Calculator();
 
         System.out.print("Enter radius: ");
         double radius = sc.nextDouble();
 
-        double c = calculator.circunference(radius);
+        double c = Calculator.circunference(radius);
 
-        double v = calculator.volume(radius);
+        double v = Calculator.volume(radius);
 
         System.out.printf("Circumference : %.2f%n", c);
         System.out.printf("Volume: %.2f%n", v);
-        System.out.printf("PI value: %.2f%n", calculator.PI);
+        System.out.printf("PI value: %.2f%n", Calculator.PI);
 
     }
 
