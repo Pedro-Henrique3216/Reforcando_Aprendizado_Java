8f1d2a1968215b9d7c4e7796984162d2ecafa97a
Pedro Henrique <ph6704938@gmail.com>
2023-08-31 10:53:17 -0300

    Transformando metodos e atributos da classe Calculator em metodos e atributos estaticos

--- src/exemplosDeAula/util/Calculator.java
+++ src/exemplosDeAula/util/Calculator.java
@@ -2,13 +2,13 @@ package exemplosDeAula.util;
 
 public class Calculator {
 
-    public final double PI = 3.14159;
+    public static final double PI = 3.14159;
 
-    public double circunference(double radius){
+    public static double circunference(double radius){
         return 2 * PI * radius;
     }
 
-    public double volume(double radius){
+    public static double volume(double radius){
         return 4 * PI * Math.pow(radius, 3) / 3;
     }
 }
