6f19cb97b8147b59ac4a7213ddae45bc7eb48d57
Pedro Henrique <ph6704938@gmail.com>
2023-08-31 23:17:48 -0300

    Criando classe de teste e1 dentro do package de exercios de metodos estaticos

---
+++ src/exerciciosDeFixacao/metodosEstaticos/e1.java
@@ -0,0 +1,21 @@
+package exerciciosDeFixacao.metodosEstaticos;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class e1 {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("What is the dollar price? ");
+        double dolar = sc.nextDouble();
+        System.out.print("How many dollars will be bought? ");
+        double quanti = sc.nextDouble();
+
+
+
+    }
+}
