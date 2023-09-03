41bd1445d04d8439c4b50cf2be6efeacde5639c4
Pedro Henrique <ph6704938@gmail.com>
2023-09-02 23:49:25 -0300

    Criando classe e1 para testar a conta

---
+++ src/exerciciosDeFixacao/construtoresSobrecargaEncapsulamento/e1.java
@@ -0,0 +1,21 @@
+package exerciciosDeFixacao.construtoresSobrecargaEncapsulamento;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class e1 {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter account number: ");
+
+
+
+
+
+
+    }
+}
