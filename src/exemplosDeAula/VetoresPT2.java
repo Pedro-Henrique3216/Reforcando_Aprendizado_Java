a36de92d8a6e938ef47ed7337da95517939e18d2
Pedro Henrique <ph6704938@gmail.com>
2023-09-03 17:33:07 -0300

    Criando classe VetoresPT, para testar o array com outra classe, exemplo Product

---
+++ src/exemplosDeAula/VetoresPT2.java
@@ -0,0 +1,37 @@
+package exemplosDeAula;
+
+import exemplosDeAula.entities.Product;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class VetoresPT2 {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        Product[] vect = new Product[n];
+
+        for(int i = 0; i < n; i++){
+            sc.nextLine();
+            String name = sc.nextLine();
+            double price = sc.nextDouble();
+            vect[i] = new Product(name, price);
+        }
+
+        double sum = 0;
+
+        for(int i = 0; i < n; i++){
+            sum += vect[i].getPrice();
+        }
+
+        double media = sum / n;
+
+        System.out.printf("AVERAGE HEIGHT %.2f%n", media);
+        sc.close();
+    }
+}
