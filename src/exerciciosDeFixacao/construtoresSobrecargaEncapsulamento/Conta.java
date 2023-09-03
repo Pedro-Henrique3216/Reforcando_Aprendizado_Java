2c348068e1f62d2e09a00850470c01b9c0a3e030
Pedro Henrique <ph6704938@gmail.com>
2023-09-03 00:22:03 -0300

    Testando metodos e construtores da Conta e arrumando o metodo saque

--- src/exerciciosDeFixacao/construtoresSobrecargaEncapsulamento/Conta.java
+++ src/exerciciosDeFixacao/construtoresSobrecargaEncapsulamento/Conta.java
@@ -38,7 +38,7 @@ public class Conta {
     }
 
     public void saque(double valor){
-        saldo -= valor - 5;
+        saldo -= (valor + 5);
     }
 
     @Override
--- src/exerciciosDeFixacao/construtoresSobrecargaEncapsulamento/e1.java
+++ src/exerciciosDeFixacao/construtoresSobrecargaEncapsulamento/e1.java
@@ -11,6 +11,38 @@ public class e1 {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter account number: ");
+        String numero = sc.nextLine();
+        System.out.print("Enter account holder: ");
+        String titular = sc.nextLine();
+        System.out.print("Is there na initial deposit (y/n)? ");
+        String temOuNao = sc.next();
+        temOuNao = temOuNao.toLowerCase();
+
+        Conta conta;
+
+        if(temOuNao.equals("y")){
+            System.out.print("Enter initial deposit value: ");
+            double deposito = sc.nextDouble();
+            conta = new Conta(numero, titular, deposito);
+        } else {
+            conta = new Conta(numero, titular);
+        }
+
+        System.out.println();
+        System.out.println("Account data:");
+        System.out.println(conta);
+
+        System.out.println();
+        System.out.print("Enter a deposit value: ");
+        conta.deposito(sc.nextDouble());
+        System.out.println("Update account data:");
+        System.out.println(conta);
+
+        System.out.println();
+        System.out.print("Enter a withdraw value: ");
+        conta.saque(sc.nextDouble());
+        System.out.println("Update account data:");
+        System.out.println(conta);
 
 
 
