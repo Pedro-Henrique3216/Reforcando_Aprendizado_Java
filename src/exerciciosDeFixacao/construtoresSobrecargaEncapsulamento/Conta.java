c1ca68293a821d991822de4ace57af956268b1b6
Pedro Henrique <ph6704938@gmail.com>
2023-09-02 23:46:24 -0300

    Criando classe Conta, atributos numeroDaConta, titular, saldo, construtores, getters e setters e metodos deposito e saque

---
+++ src/exerciciosDeFixacao/construtoresSobrecargaEncapsulamento/Conta.java
@@ -0,0 +1,44 @@
+package exerciciosDeFixacao.construtoresSobrecargaEncapsulamento;
+
+public class Conta {
+
+    private String numeroDaConta;
+    private String titular;
+    private double saldo;
+
+    public Conta(String numeroDaConta, String titular){
+        this.numeroDaConta = numeroDaConta;
+        this.titular = titular;
+    }
+
+    public Conta(String numeroDaConta, String titular, double saldo){
+        this.numeroDaConta = numeroDaConta;
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+
+    public String getNumeroDaConta(){
+        return numeroDaConta;
+    }
+
+    public String getTitular(){
+        return titular;
+    }
+
+    public void setTitular(String titular){
+        this.titular = titular;
+    }
+
+    public double getSaldo(){
+        return saldo;
+    }
+
+    public void deposito(double valor){
+        saldo += valor;
+    }
+
+    public void saque(double valor){
+        saldo -= valor - 5;
+    }
+
+}
