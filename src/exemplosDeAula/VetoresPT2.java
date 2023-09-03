6dd63a15a2cde561c27c8e90623fc3de4c4ba785
Pedro Henrique <ph6704938@gmail.com>
2023-09-03 17:34:42 -0300

    Trocando o n for para vect.length

--- src/exemplosDeAula/VetoresPT2.java
+++ src/exemplosDeAula/VetoresPT2.java
@@ -16,7 +16,7 @@ public class VetoresPT2 {
 
         Product[] vect = new Product[n];
 
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < vect.length; i++){
             sc.nextLine();
             String name = sc.nextLine();
             double price = sc.nextDouble();
@@ -25,7 +25,7 @@ public class VetoresPT2 {
 
         double sum = 0;
 
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < vect.length; i++){
             sum += vect[i].getPrice();
         }
 
