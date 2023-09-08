07c1b9cd431597a8225206e48882ef1e6f74922b
Pedro Henrique <ph6704938@gmail.com>
2023-09-08 14:02:40 -0300

    Codigo criado para calcular a media dos numeros par de um array,

--- src/exerciciosDeFixacao/vetores/e8.java
+++ src/exerciciosDeFixacao/vetores/e8.java
@@ -1,10 +1,35 @@
 package exerciciosDeFixacao.vetores;
 
+import java.util.Scanner;
+
 public class e8 {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Quantos elementos vai ter o vetor? ");
+        int quantidade = sc.nextInt();
+        double media;
+        int somaPares = 0;
 
+        int[] numeros = new int[quantidade];
+
+        for(int i = 0; i < numeros.length; i++){
+            System.out.print("Digite um numero: ");
+            numeros[i] = sc.nextInt();
+            if(numeros[i] % 2 == 0){
+                somaPares += numeros[i];
+            }
+        }
+
+        if(somaPares > 0){
+            media = (double) somaPares / quantidade;
+            System.out.printf("Media dos pares = %.1f", media);
+        } else {
+            System.out.println("Nenhum numero par");
+        }
+
+        sc.close();
     }
 }
