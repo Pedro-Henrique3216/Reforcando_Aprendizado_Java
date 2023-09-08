8a4ecd2d21901b08c4cfc2d4dbcadb1b6bdfe411
Pedro Henrique <ph6704938@gmail.com>
2023-09-08 19:54:15 -0300

    Fazendo a mesma verificação, porem com a classe aluno

--- src/exerciciosDeFixacao/vetores/e10.java
+++ src/exerciciosDeFixacao/vetores/e10.java
@@ -13,29 +13,26 @@ public class e10 {
         System.out.print("Quantos alunos serao digitados? ");
         int quantidade = sc.nextInt();
 
-        String[] alunos = new String[quantidade];
-        double[] pSemestre = new double[quantidade];
-        double[] sSemestre = new double[quantidade];
+        Aluno[] alunos = new Aluno[quantidade];
 
-        for(int i = 0; i < quantidade; i++) {
+        for(int i = 0; i < alunos.length; i++) {
             System.out.printf("Digite nome, primeira e segunda nota do %do aluno: \n", i + 1);
             sc.nextLine();
-            alunos[i] = sc.nextLine();
-            pSemestre[i] = sc.nextDouble();
-            sSemestre[i] = sc.nextDouble();
+            String nome = sc.nextLine();
+            double pSemestre = sc.nextDouble();
+            double sSemestre = sc.nextDouble();
+
+            alunos[i] = new Aluno(nome, pSemestre, sSemestre);
         }
 
-        System.out.println("Alunos aprovados");
-        for(int i = 0; i < quantidade; i++){
-            double sum = pSemestre[i] + sSemestre[i];
+        System.out.println("Alunos aprovados: ");
+        for(int i = 0; i < alunos.length; i++){
+            double sum = alunos[i].getNotaPSemestre() + alunos[i].getNotaSSemestre();
             double media = sum / 2;
             if(media >= 6){
-                System.out.println(alunos[i]);
+                System.out.println(alunos[i].getNome());
             }
         }
-
-
-
 
         sc.close();
     }
