d3e7e18473c30b76b20eeb9f3116f7f832d24153
Pedro Henrique <ph6704938@gmail.com>
2023-09-08 19:43:44 -0300

    criando classe e10 onde verifica qua aluno foi aprovado

---
+++ src/exerciciosDeFixacao/vetores/e10.java
@@ -0,0 +1,43 @@
+package exerciciosDeFixacao.vetores;
+
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class e10 {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Quantos alunos serao digitados? ");
+        int quantidade = sc.nextInt();
+
+        String[] alunos = new String[quantidade];
+        double[] pSemestre = new double[quantidade];
+        double[] sSemestre = new double[quantidade];
+
+        for(int i = 0; i < quantidade; i++) {
+            System.out.printf("Digite nome, primeira e segunda nota do %do aluno: \n", i + 1);
+            sc.nextLine();
+            alunos[i] = sc.nextLine();
+            pSemestre[i] = sc.nextDouble();
+            sSemestre[i] = sc.nextDouble();;
+        }
+
+        System.out.println("Alunos aprovados");
+        for(int i = 0; i < quantidade; i++){
+            double sum = pSemestre[i] + sSemestre[i];
+            double media = sum / 2;
+            if(media >= 6){
+                System.out.println(alunos[i]);
+            }
+        }
+
+
+
+
+        sc.close();
+    }
+}
