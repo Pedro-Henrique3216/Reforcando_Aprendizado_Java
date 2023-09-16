3c323085832edba0188a6973004cb29f390a994e
Pedro Henrique <ph6704938@gmail.com>
2023-09-16 20:32:48 -0300

    exemplo de instanciamento de datas e horas

---
+++ src/exemplosDeAula/InstanciarDataEHora.java
@@ -0,0 +1,46 @@
+package exemplosDeAula;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class InstanciarDataEHora {
+
+    public static void main(String[] args) {
+
+        LocalDate d01 = LocalDate.now();
+        System.out.println(d01);
+
+        LocalDateTime d02 = LocalDateTime.now();
+        System.out.println(d02);
+
+        Instant d03 = Instant.now();
+        System.out.println(d03);
+
+        LocalDate d04 = LocalDate.parse("2022-07-20");
+        System.out.println(d04);
+
+        LocalDateTime d05 = LocalDateTime.parse("2022-07-20T01:30:26");
+        System.out.println(d05);
+
+        Instant d06 = Instant.parse("2022-07-20T01:30:26Z");
+        System.out.println(d06);
+
+        Instant d07 = Instant.parse("2022-07-20T01:30:26-03:00");
+        System.out.println(d07);
+
+        LocalDate d08 = LocalDate.parse("20/07/2022", DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        System.out.println(d08);
+
+        DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        LocalDateTime d09 = LocalDateTime.parse("20/07/2022 01:30", fmt2);
+        System.out.println(d09);
+
+        LocalDate d10 = LocalDate.of(2022, 7, 20);
+        System.out.println(d10);
+
+        LocalDateTime d11 = LocalDateTime.of(2022, 7, 20, 1, 30);
+        System.out.println(d11);
+    }
+}
