810266570dad4a43cf939985bd281c1f29d44f91
Pedro Henrique <ph6704938@gmail.com>
2023-09-29 19:25:10 -0300

    exemplo do bloco Finally

---
+++ src/exemplosDeAula/BlocoFinally.java
@@ -0,0 +1,30 @@
+package exemplosDeAula;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class BlocoFinally {
+
+    public static void main(String[] args) {
+
+        File flie = new File("C:\\temp\\in.txt");
+        Scanner sc = null;
+
+        try {
+            sc = new Scanner(flie);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+        } catch (FileNotFoundException e){
+            System.out.println("Error opening file: " + e.getMessage());
+        } finally {
+            if(sc != null){
+                sc.close();
+            }
+
+            System.out.println("Finally block executed");
+        }
+    }
+
+}
