7cc43a5243be8afd1812ad754219081c099bfffe
Pedro Henrique <ph6704938@gmail.com>
2023-10-02 09:49:00 -0300

    First class: Position

---
+++ src/projetoChess_System/applications/Progam.java
@@ -0,0 +1,12 @@
+package projetoChess_System.applications;
+
+import projetoChess_System.boardgame.Position;
+
+public class Progam {
+
+    public static void main(String[] args) {
+
+        Position pos= new Position(3, 5);
+        System.out.println(pos);
+    }
+}
