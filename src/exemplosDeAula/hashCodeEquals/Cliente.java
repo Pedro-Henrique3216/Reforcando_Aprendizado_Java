eb6cd8662fc710dbb340f3bbc6df58e5bdbaa18c
Pedro Henrique <ph6704938@gmail.com>
2023-10-19 10:55:59 -0300

    exemplo de equals e hashCode

---
+++ src/exemplosDeAula/hashCodeEquals/Cliente.java
@@ -0,0 +1,27 @@
+package exemplosDeAula.hashCodeEquals;
+
+import java.util.Objects;
+
+public class Cliente {
+
+    private String name;
+    private String email;
+
+    public Cliente(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return Objects.equals(name, cliente.name) && Objects.equals(email, cliente.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, email);
+    }
+}
