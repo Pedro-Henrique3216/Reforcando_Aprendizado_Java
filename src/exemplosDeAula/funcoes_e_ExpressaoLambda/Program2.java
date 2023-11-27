e65156ab97a47b2e682de8a3608636aefeac995c
Pedro Henrique <ph6704938@gmail.com>
2023-11-27 09:37:57 -0300

    exemplo de como fazer uma função que implementa outra função

---
+++ src/exemplosDeAula/funcoes_e_ExpressaoLambda/Program2.java
@@ -0,0 +1,25 @@
+package exemplosDeAula.funcoes_e_ExpressaoLambda;
+
+import exemplosDeAula.Product;
+import exemplosDeAula.function.ProductService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program2 {
+
+    public static void main(String[] args) {
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("TV", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.50));
+        list.add(new Product("HD Case", 80.90));
+
+        ProductService ps = new ProductService();
+        double sum =  ps.filteredSum(list, p -> p.getPrice() < 100);
+
+        System.out.println("Sum : " + String.format("%.2f", sum));
+    }
+}
---
+++ src/exemplosDeAula/function/ProductService.java
@@ -0,0 +1,20 @@
+package exemplosDeAula.function;
+
+import exemplosDeAula.Product;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+public class ProductService {
+
+    public double filteredSum(List<Product> list, Predicate<Product> criteria) {
+        double sum = 0;
+        for(Product p : list){
+            if(criteria.test(p)){
+                sum += p.getPrice();
+            }
+        }
+
+        return sum;
+    }
+}
